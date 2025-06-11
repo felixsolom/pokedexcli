388c561f91ca7b52b420b2c8934d2ef2eb2706d1
felixsolom <felix.solom@gmail.com>
2025-06-12 00:46:02 +0300

    start repl

--- repl.go
+++ repl.go
@@ -1,6 +1,23 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := fmt.Sprintf(scanner.Text())
+		clean_slice := cleanInput(input)
+		first_word := clean_slice[0]
+		fmt.Printf("Your command was: %s\n", first_word)
+	}
+}
 
 func cleanInput(text string) []string {
 	lower_str := strings.ToLower(text)
